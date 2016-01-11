1ed88a12a9dbcb9ac2497049cf09ec18d7ea2419
Konstantin Hadzhiev <koko.hadzhiev@gmail.com>
2016-01-11 15:46:44 +0100

    Map updates location
    :)

--- test/www/js/controllers/ContentController.js
+++ test/www/js/controllers/ContentController.js
@@ -2,22 +2,18 @@ app.controller('ContentCtrl', function($scope, $log, $ionicLoading, $cordovaGeol
 angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'});
   // $scope.nickname = $stateParams.data.nickname;
   // $scope.displayPicture = $stateParams.data.displayPicture;
-
   // var headers = {
   //       'Access-Control-Allow-Origin' : '*',
   //       'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
   //       'Content-Type': 'application/json',
   //       'Accept': 'application/json'
   //     };
-
       
-
   // //console.log(headers);
   // // $http.get("http://api.deezer.com/artist/27")
   // //.then(function (response) {$scope.names = response.data; alert(JSON.stringify($scope.names));});
   // $http({method: 'GET', url: 'http://api.deezer.com/artist/27', headers: headers
   // });
-
 // $http({method: 'GET', url: 'http://djangounchained-dechochernev.c9users.io/api/v1/events/?format=json'})
 //       .success(function(data, status, headers, config){
 //         $scope.ads = data;
@@ -26,15 +22,12 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
 //       .error(function(data, status, headers, config){
 //         $log.warn(data);
 //       })
-
 // var pos = {
 //                 enableHighAccuracy: true
 //             };
-
 //           $cordovaGeolocation.getCurrentPosition(pos).then(function(pos) {
 //             var lat = pos.coords.latitude;
 //             var long = pos.coords.longitude;
-
 //                 $scope.position = new google.maps.LatLng(lat, long);
 //                 console.log(JSON.stringify($scope.position));
 //                 var mapOptions = {
@@ -50,33 +43,28 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
 // //BEGIN
 // //                 var input = /** @type {HTMLInputElement} */(
 // //       document.getElementById('pac-input'));
-
 // //   // Create the autocomplete helper, and associate it with
 // //   // an HTML text input box.
 // //   var autocomplete = new google.maps.places.Autocomplete(input);
 // //   autocomplete.bindTo('bounds', map);
-
 // // google.maps.event.addListener(autocomplete, 'place_changed', function() {
 // //     //infowindow.close();
 // //     var place = autocomplete.getPlace();
 // //     if (!place.geometry) {
 // //       return;
 // //     }
-
 // //     if (place.geometry.viewport) {
 // //       map.fitBounds(place.geometry.viewport);
 // //     } else {
 // //       map.setCenter(place.geometry.location);
 // //       map.setZoom(17);
 // //     }
-
 // //     // Set the position of the marker using the place ID and location.
 // //     marker.setPlace(/** @type {!google.maps.Place} */ ({
 // //       placeId: place.place_id,
 // //       location: place.geometry.location
 // //     }));
 // //     marker.setVisible(true);
-
 // //     //infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
 // //         //'Place ID: ' + place.place_id + '<br>' +
 // //        // place.formatted_address + '</div>');
@@ -88,20 +76,16 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
 //                     position: $scope.position,
 //                     tite: 'Football'
 //                 });
-
 //                 marker.setMap(map);
 //             }, 
 //             function(error) {  
 //                   $ionicLoading.hide();                  
 //                   alert('Unable to get location: ' + error.message);
 //               }, pos);
-
-
   $ionicSideMenuDelegate.canDragContent(true);
   $scope.toggleNav = function(){
     $ionicSideMenuDelegate.toggleLeft();
   }
-
   $ionicModal.fromTemplateUrl('templates/new-event.html', {
     scope: $scope,
     animation: 'slide-in-up'
@@ -114,11 +98,9 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
     var pos = {
                 enableHighAccuracy: true
             };
-
           $cordovaGeolocation.getCurrentPosition(pos).then(function(pos) {
             var lat = pos.coords.latitude;
             var long = pos.coords.longitude;
-
                 $scope.position = new google.maps.LatLng(lat, long);
                 console.log(JSON.stringify($scope.position));
                 var mapOptions = {
@@ -131,23 +113,31 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
                 console.log(map);
                 $scope.map = map;  
                 $ionicLoading.hide();
-
                  map.addListener('click', function(event){
-
                   var lat = event.latLng.lat();
                   var lng = event.latLng.lng();
-
                   // marker = new google.maps.Marker({
                   //     map: $scope.map,
                   //     position: new google.maps.LatLng(lat, lng)
                   // });
-
-                  marker.setPosition(event.latLng);
+                  // marker.setPosition(event.latLng);
                   var input = /** @type {HTMLInputElement} */(
       document.getElementById('pac-input'));
-                  input.value = event.latLng;
-
-
+                     $scope.geocoder.geocode({'location': event.latLng}, function(results, status) {
+                    if (status === google.maps.GeocoderStatus.OK) {
+                      if (results[1]) {
+                        marker.setPosition(event.latLng);
+                        input.value = results[0].formatted_address;
+                        // infowindow.setContent(results[1].formatted_address);
+                        // infowindow.open(map, marker);
+                      } else {
+                        window.alert('No results found');
+                      }
+                    } else {
+                      window.alert('Geocoder failed due to: ' + status);
+                    }
+                  });
+                  // input.value = event.latLng;
                   // if($scope.map.marker) $scope.map.marker.setMap(null);
                   // if($scope.map.marker) delete $scope.map.marker;
                   // $scope.map.marker = new google.maps.Marker({
@@ -155,9 +145,7 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
                   //     position: new google.maps.LatLng(lat, lng)
                   // })
                   // marker.setMap(null);
-
                   }) 
-
                 $scope.geocoder = new google.maps.Geocoder();
                 $scope.geocodeLatLng = function(geocoder, map) {
                   var input = document.getElementById('pac-input');
@@ -186,35 +174,29 @@ angular.element(document.querySelector('#map2')).css({'border':'3px solid blue'}
 //BEGIN
                 var input = /** @type {HTMLInputElement} */(
       document.getElementById('pac-input'));
-
                 // input.value= $scope.position;
-
   // Create the autocomplete helper, and associate it with
   // an HTML text input box.
   var autocomplete = new google.maps.places.Autocomplete(input);
   autocomplete.bindTo('bounds', map);
-
 google.maps.event.addListener(autocomplete, 'place_changed', function() {
     //infowindow.close();
     var place = autocomplete.getPlace();
     if (!place.geometry) {
       return;
     }
-
     if (place.geometry.viewport) {
       map.fitBounds(place.geometry.viewport);
     } else {
       map.setCenter(place.geometry.location);
       map.setZoom(17);
     }
-
     // Set the position of the marker using the place ID and location.
     marker.setPlace(/** @type {!google.maps.Place} */ ({
       placeId: place.place_id,
       location: place.geometry.location
     }));
     marker.setVisible(true);
-
     //infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
         //'Place ID: ' + place.place_id + '<br>' +
        // place.formatted_address + '</div>');
@@ -226,28 +208,22 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
                     position: $scope.position,
                     tite: 'Football'
                 });
-
                 marker.setMap(map);
             }, 
             function(error) {  
                   $ionicLoading.hide();                  
                   alert('Unable to get location: ' + error.message);
               }, pos);
-
   }
-
   $scope.closeModal = function(){
     $scope.modal.hide();
   }
-
   $scope.getEvents = function(){EventsService.getAllEvents().then(function(resp){
       $scope.events = resp.data;
       $scope.event = EventsService.get($stateParams.eventId, $scope.events);
     });
   }
-
   $scope.getEvents();
-
   $scope.doRefresh = function(){
     $http.get('http://djangounchained-dechochernev.c9users.io/api/v1/events/')
       .success(function(newEvents){
@@ -257,7 +233,6 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
         $scope.$broadcast('scroll.refreshComplete');
       })
   }
-
   $scope.takePhoto = function () {
     var options = {
       quality: 75,
@@ -270,14 +245,12 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
       popoverOptions: CameraPopoverOptions,
       saveToPhotoAlbum: false
     };
-
     $cordovaCamera.getPicture(options).then(function (imageData) {
       $scope.imgURI = "data:image/jpeg;base64," + imageData;
     }, function (err) {
   // An error occured. Show a message to the user
 });
   }
-
   $scope.choosePhoto = function () {
     var options = {
       quality: 75,
@@ -290,15 +263,12 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
       popoverOptions: CameraPopoverOptions,
       saveToPhotoAlbum: false
     };
-
     $cordovaCamera.getPicture(options).then(function (imageData) {
       $scope.imgURI = "data:image/jpeg;base64," + imageData;
     }, function (err) {
   // An error occured. Show a message to the user
 });
   }
-
-
   $scope.myEvents = [
     {
       id: 3,
@@ -308,7 +278,6 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
       image: 'http://placehold.it/100x100'
     }
   ];
-
    $scope.disableTap = function(){
     container = document.getElementsByClassName('pac-container');
     // disable ionic data tab
@@ -318,7 +287,6 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
         document.getElementById('pac-input').blur();
     });
   }
-
   $scope.sendData = function(event){
     $http({
       url: 'http://djangounchained-dechochernev.c9users.io/api/event_list/',
@@ -338,11 +306,9 @@ google.maps.event.addListener(autocomplete, 'place_changed', function() {
         alert(event.name + " created successfully!"); 
     });
   }
-
   $scope.removeEvent = function(index){
     $scope.myEvents.splice($scope.events[index],1);
   }
-
   $scope.addEvent = function(index){
       $scope.myEvents.push($scope.events[index]);  
     console.log($scope.myEvents);
